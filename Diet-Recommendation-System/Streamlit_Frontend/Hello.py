4617f7af260784b5934b0ee09cf8d7a018d110e5
Neha Sharma <ns1508neha@gmail.com>
2025-11-21 22:40:05 +0530

    Clean project without submodule

---
+++ Diet-Recommendation-System/Streamlit_Frontend/Hello.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Nutri-Choice! 👋")
+
+st.sidebar.success("Select a recommendation app.")
+
+st.markdown(
+    """
+    A diet recommendation web application using content-based approach with Scikit-Learn, FastAPI and Streamlit.
+    You can find more details and the whole project on my [repo](https://https://github.com/Nehasharma76/Nutri-Choice).
+    """    
+)
